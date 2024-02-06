4d6f5f65a2b8dfe50a20faf6922cd343843d007b
pastifra <francescopasti98@gmail.com>
2024-02-06 12:22:03 +0100

    Working pipeline for GPU training

--- nanodet/model/arch/nanodet_plus.py
+++ nanodet/model/arch/nanodet_plus.py
@@ -25,21 +25,22 @@ class NanoDetPlus(OneStageDetector):
         self,
         backbone,
         fpn,
-        aux_head,
+        #aux_head,
         head,
         detach_epoch=0,
     ):
         super(NanoDetPlus, self).__init__(
             backbone_cfg=backbone, fpn_cfg=fpn, head_cfg=head
         )
-        self.aux_fpn = copy.deepcopy(self.fpn)
-        self.aux_head = build_head(aux_head)
+        #self.aux_fpn = copy.deepcopy(self.fpn)
+        #self.aux_head = build_head(aux_head)
         self.detach_epoch = detach_epoch
 
     def forward_train(self, gt_meta):
         img = gt_meta["img"]
         feat = self.backbone(img)
         fpn_feat = self.fpn(feat)
+        '''
         if self.epoch >= self.detach_epoch:
             aux_fpn_feat = self.aux_fpn([f.detach() for f in feat])
             dual_fpn_feat = (
@@ -51,7 +52,8 @@ class NanoDetPlus(OneStageDetector):
             dual_fpn_feat = (
                 torch.cat([f, aux_f], dim=1) for f, aux_f in zip(fpn_feat, aux_fpn_feat)
             )
+        '''
         head_out = self.head(fpn_feat)
-        aux_head_out = self.aux_head(dual_fpn_feat)
-        loss, loss_states = self.head.loss(head_out, gt_meta, aux_preds=aux_head_out)
+        #aux_head_out = self.aux_head(dual_fpn_feat)
+        loss, loss_states = self.head.loss(head_out, gt_meta) #aux_preds=aux_head_out)
         return head_out, loss, loss_states
